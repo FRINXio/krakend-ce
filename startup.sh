50b178fdc9929b4a4dc91c3d93c0997f139a8913
Jozef Volak <jvolak@frinx.io>
2022-03-29 10:08:45 +0200

    Proxy environment configuration

--- startup.sh
+++ startup.sh
@@ -7,4 +7,8 @@ if [ -f "/set_env_secrets.sh" ]; then
   . /set_env_secrets.sh ''
 fi
 
+if [ "PROXY_ENABLED" != 'true' ]; then
+  unset HTTP_PROXY HTTPS_PROXY NO_PROXY
+fi
+
 /usr/bin/krakend run -c /etc/krakend/krakend.json
