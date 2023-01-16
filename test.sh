aed1e6d00725557d6415c8ba88a69c726e0d8fa3
Jozef Volak <jvolak@frinx.io>
2023-01-16 12:40:11 +0100

    Fix pull private module in github actions

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export GOPRIVATE="github.com/FRINXio/krakend-websocket"
-git config --global url."https://${GH_TOKEN}:x-oauth-basic@github.com/FRINXio".insteadOf "https://github.com/FRINXio"
-go mod tidy
-make build
-go test -v ./tests
